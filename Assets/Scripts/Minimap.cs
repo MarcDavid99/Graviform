e859f7edc45c8e4727bf0fa4e805e27b08dae72d
Fa1nt <55049913+Fa1nt@users.noreply.github.com>
2020-01-15 16:32:56 +0200

    minimap (toggle w/ TAB) for level 3

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Minimap : MonoBehaviour
+{
+    public RawImage testImage;
+    private bool imgOn;
+
+    // Use this for initialization
+    void Start()
+    {
+        testImage.enabled = false;
+        imgOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (imgOn == true)
+            {
+                testImage.enabled = false;
+                imgOn = false;
+            }
+
+            else
+            {
+                testImage.enabled = true;
+                imgOn = true;
+            }
+        }
+
+    }
+}
